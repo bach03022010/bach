e431b5630066f5c98dbdfb4d6280348b2f071c6a
bach03022010 <91815366+bach03022010@users.noreply.github.com>
2021-10-17 07:13:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Bỏ Block tao đi nhoa🤗😅`,
+    btnIntro: 'OK',
+    title: 'Bỏ Block tao đi nhoa',
+    desc: 'Mày không trả lời hoặc thoát ra, tức là mày bỏ block rồi nhá',
+    btnYes: 'Có🤗🤗',
+    btnNo: ' mơ à !😢😢 ',
+    question:'Nói cho Tao biết đi vì sao bỏ block tao 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì Bách đẹp zai, code giỏi ...vv Ahiiiii 😊😊',
+    mess: 'Tao biết mà, bỏ block tao đê',
+    messDesc: '💬💬 Bỏ block bố mày nhanh lên💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://zalo.me/0369994802'
+}
